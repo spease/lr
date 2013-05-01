2a98d369a8e253ce2b54578db7a499cac1e877f5
Steven Pease <peasteven@gmail.com>
2013-04-30 20:47:29 -0700

    Remove old broken code

--- LRParser.hpp
+++ LRParser.hpp
@@ -25,9 +25,6 @@ public:
   bool setGrammar(Grammar const * const i_grammarPointer);
 
   static SymbolMap buildFirst(Grammar const &i_grammar);
-  //static SymbolMap buildFirst(Grammar const * const i_grammarPointer);
-  //static SymbolList buildFirstNonterminal(Grammar const * const i_grammarPointer, Symbol const &i_symbol);
-  //static SymbolList buildFirstProduction(Grammar const * const i_grammarPointer, Production const &p);
   static SymbolMap buildFollow(Grammar const * const i_grammarPointer);
 private:
   LRParser(LRParser const &)=delete;
