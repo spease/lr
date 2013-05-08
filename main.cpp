98d44338dd97e2703d3690cc1b87f7959768d1ab
Steven Pease <peasteven@gmail.com>
2013-05-07 17:23:06 -0700

    Use lec12 grammar for testing

--- main.cpp
+++ main.cpp
@@ -14,6 +14,14 @@ int main(int const argc, char const * const * const argv)
   g |= NT("term") >>= NT("factor") + T("*") + NT("term") | NT("factor");
   g |= NT("factor") >>= T("0") | T("1") | T("2") | T("3") | T("4") | T("5") | T("6") | T("7") | T("8") | T("9");
   */
+
+  g |= NT("goal") >>= NT("expr");
+  g |= NT("expr") >>= NT("term") + T("+") + NT("expr");
+  g |= NT("expr") >>= NT("term");
+  g |= NT("term") >>= NT("factor") + T("*") + NT("term");
+  g |= NT("term") >>= NT("factor");
+  g |= NT("factor") >>= T("id");
+
   /*
   g |= NT("E") >>= NT("E") + T("*") + NT("B");
   g |= NT("E") >>= NT("E") + T("+") + NT("B");
@@ -21,13 +29,17 @@ int main(int const argc, char const * const * const argv)
   g |= NT("B") >>= T("0");
   g |= NT("B") >>= T("1");
   */
+
   //g |= NT("B") >>= T("hello") + T(" ") + T("world") | T("goodbye") + T(" ") + T("world");
+
+  /*
   g |= NT("start_$") >>= NT("start");
   g |= NT("start") >>= NT("start") + NT("expr");
   g |= NT("start");
   g |= NT("expr") >>= T("NR");
   g |= NT("expr") >>= NT("expr") + T("+") + NT("expr");
-  
+  */
+
   LRParser p;
   bool pResult = p.setGrammar(&g);
   if(!pResult)
