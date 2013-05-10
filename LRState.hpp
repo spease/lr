1ec6a6340e116739401e1e8a4f059c1c734b4977
Steven Pease <peasteven@gmail.com>
2013-05-09 21:52:00 -0700

    Add LRState

---
+++ LRState.hpp
@@ -0,0 +1,12 @@
+#ifndef _LRSTATE_HPP_
+#define _LRSTATE_HPP_
+
+#include <cstddef>
+#include <stack>
+
+/********************----- CLASS: LRState -----********************/
+typedef std::size_t LRState;
+typedef std::stack<LRState> LRStateStack;
+/**************************************************/
+
+#endif /* _LRSTATE_HPP_ */
