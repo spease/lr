f62b55146de3e0007e0f135887c9ae8e7b9e3d81
Steven Pease <peasteven@gmail.com>
2013-06-06 05:33:22 -0700

    Fix ENUM_GENERATE_HELPERS

--- global.hpp
+++ global.hpp
@@ -12,11 +12,11 @@ constexpr typename std::underlying_type<T>::type enum_value(T val)
 }
 
 #define ENUM_GENERATE_HELPERS(x)  \
-  inline x operator|=(x const i_a, x const i_b){return static_cast<x>(enum_value(i_a)|enum_value(i_b));} \
-  inline x operator&=(x const i_a, x const i_b){return static_cast<x>(enum_value(i_a)|enum_value(i_b));} \
+  inline x operator|=(x &i_a, x const i_b){return (i_a = static_cast<x>(enum_value(i_a)|enum_value(i_b)));} \
+  inline x operator&=(x &i_a, x const i_b){return (i_a = static_cast<x>(enum_value(i_a)|enum_value(i_b)));} \
   inline x operator~(x const i_a){return static_cast<x>(~enum_value(i_a));}                              \
   inline x operator|(x const i_a, x const i_b){return static_cast<x>(enum_value(i_a)|enum_value(i_b));}  \
-  inline x operator&(x const i_a, x const i_b){return static_cast<x>(enum_value(i_a)|enum_value(i_b));}  \
+  inline x operator&(x const i_a, x const i_b){return static_cast<x>(enum_value(i_a)&enum_value(i_b));}  \
   inline bool operator!(x const i_a){return !(enum_value(i_a) ? true : false);}
 
 /**************************************************/
